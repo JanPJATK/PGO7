317597871d079a1c90a59eea0bff2e002f45971b
JanPJATK <s32026@pjwstk.edu.pl>
2024-06-23 20:37:55 +0200

    item

--- item/src/item.java
+++ item/src/item.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 class Item {
     private int id;
@@ -23,32 +23,37 @@ class Item {
     public void show() {
         System.out.println(name);
     }
+
+    @Override
+    public String toString() {
+        return "Item{id=" + id + ", name='" + name + "'}";
+    }
 }
 
 public class item {
     public static void main(String[] args) {
-
+        // Tworzenie listy ArrayList i dodawanie obiektów klasy Item
         List<Item> itemList = new ArrayList<>();
         itemList.add(new Item(1, "Laptop"));
         itemList.add(new Item(2, "Smartphone"));
         itemList.add(new Item(3, "Tablet"));
         itemList.add(new Item(4, "Smartwatch"));
         itemList.add(new Item(5, "Camera"));
+        itemList.add(new Item(6, "Headphones"));
+        itemList.add(new Item(7, "Keyboard"));
+        itemList.add(new Item(8, "Mouse"));
+        itemList.add(new Item(9, "Monitor"));
+        itemList.add(new Item(10, "Printer"));
 
+        // Wyłuskiwanie pięciu pierwszych elementów z listy za pomocą metody subList()
+        List<Item> firstFiveItems = itemList.subList(0, 5);
 
-        for (int i = 0; i < itemList.size(); i++) {
-            itemList.get(i).show();
-        }
+        // Tworzenie HashSet i dodawanie do niej pięciu pierwszych elementów z listy
+        Set<Item> itemSet = new HashSet<>(firstFiveItems);
 
-
-        Map<Integer, String> itemMap = new HashMap<>();
-        for (Item item : itemList) {
-            itemMap.put(item.getId(), item.getName());
-        }
-
-
-        for (Map.Entry<Integer, String> entry : itemMap.entrySet()) {
-            System.out.println("ID: " + entry.getKey() + ", Name: " + entry.getValue());
+        // Wyświetlanie elementów w HashSet
+        for (Item item : itemSet) {
+            System.out.println(item);
         }
     }
 }
